cf0715fc23224dbfd138c427205b0d7c4e085407
Shreyas <shreyasss94@gmail.com>
2026-03-04 15:47:08 +0100

    Model ready. Add some API next

--- Cslib/AlgorithmsTheory/Models/RobertsonWebb.lean
+++ Cslib/AlgorithmsTheory/Models/RobertsonWebb.lean
@@ -50,14 +50,54 @@ inductive RobertsonWebbQuery (α : Type) : Type → Type where
       returns a `y` such that `eval i x y = val` or `y = 1` -/
   | mark (i : α) (x : UnitI) (val : ℝ) : RobertsonWebbQuery α UnitI
 
+@[ext, grind]
+structure RWCosts where
+  evals : ℕ
+  marks : ℕ
 
+/-- Equivalence between SortOpsCost and a product type. -/
+def RWCosts.equivProd : RWCosts ≃ (ℕ × ℕ) where
+  toFun rwc := (rwc.evals, rwc.marks)
+  invFun pair := ⟨pair.1, pair.2⟩
+  left_inv _ := rfl
+  right_inv _ := rfl
+
+namespace RWCosts
+
+@[simps, grind]
+instance : Zero RWCosts := ⟨0, 0⟩
+
+@[simps]
+instance : LE RWCosts where
+  le soc₁ soc₂ := soc₁.evals ≤ soc₂.evals ∧ soc₁.marks ≤ soc₂.marks
+
+instance : LT RWCosts where
+  lt soc₁ soc₂ := soc₁ ≤ soc₂ ∧ ¬soc₂ ≤ soc₁
+
+@[grind]
+instance : PartialOrder RWCosts :=
+  fast_instance% RWCosts.equivProd.injective.partialOrder _ .rfl .rfl
+
+@[simps]
+instance : Add RWCosts where
+  add soc₁ soc₂ := ⟨soc₁.evals + soc₂.evals, soc₁.marks + soc₂.marks⟩
+
+@[simps]
+instance : SMul ℕ RWCosts where
+  smul n soc := ⟨n • soc.evals, n • soc.marks⟩
+
+instance : AddCommMonoid RWCosts :=
+  fast_instance%
+    RWCosts.equivProd.injective.addCommMonoid _ rfl (fun _ _ => rfl) (fun _ _ => rfl)
+
+end RWCosts
 
 open Classical in
 /-- This model necessarily uses classical. Watch out for hacks -/
 @[simps]
 noncomputable def RobertsonWebbModel {α : Type}
     (allocInst : AllocInstance α UnitI)
-    : Model (RobertsonWebbQuery α) ℕ where
+    : Model (RobertsonWebbQuery α) RWCosts where
   evalQuery
     | .eval i x y => (allocInst.valFuns i).val (Set.Icc x y)
     | .mark i x val =>
@@ -66,11 +106,11 @@ noncomputable def RobertsonWebbModel {α : Type}
           Exists.choose h
         else
           ⟨(1 : ℝ), by grind⟩
-  cost _ := 1
+  cost
+    | .eval _ _ _ => ⟨1, 0⟩
+    | .mark _ _ _ => ⟨0, 1⟩
 
-lemma RobertsonWebbModel.mark_leq {a : AllocInstance α UnitI}
-    (i : α) (x : UnitI) (val : ℝ)  :
-      RobertsonWebbModel.evalQuery(.eval i x (RobertsonWebbModel.evalQuery (.mark i x val))) ≤ val := by sorry
+
 end Algorithms
 
 end Cslib
